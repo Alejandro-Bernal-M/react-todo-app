b5697d36024f24662f0f66cf98e736f08c52db1a
alejandro <alejandrober96@gmail.com>
2023-03-27 11:45:22 -0500

    Creating components

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Header from './Header';
+import TodosLogic from './TodosLogic';
+
+function TodoApp() {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+}
+
+export default TodoApp;
